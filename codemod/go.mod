8b0855fd2887e8f95f267a050706b0ead886818c
Greg Weber <1183+gregwebs@users.noreply.github.com>
2022-10-11 09:16:05 -0500

    add downgrade rules

--- codemod/go.mod
+++ codemod/go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/gregwebs/errors v0.13.0 // indirect
-	github.com/gregwebs/try v1.1.0 // indirect
+	github.com/gregwebs/try v1.3.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/tools v0.1.12 // indirect
---
+++ codemod/semgrep/case/downgrade/input.go
@@ -0,0 +1,50 @@
+package ifelse
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/gregwebs/try"
+)
+
+const idxTimeFmt = "2006-01-02 15:04:05.99999"
+
+type addresses struct {
+	ID        string
+	CreatedAt string
+	UpdatedAt string
+}
+
+func (i addresses) switchParse(b []byte) (_ interface{}, err error) {
+	defer try.Handle(&err, nil)
+	var d addresses
+	err = json.Unmarshal(b, &d)
+	try.Check(err)
+	id, err := strconv.ParseInt(d.ID, 10, 64)
+	try.Check(err)
+	cr, err := time.Parse(idxTimeFmt, d.CreatedAt)
+	try.Check(err)
+	ud, err := time.Parse(idxTimeFmt, d.UpdatedAt)
+	try.Check(err)
+	s := struct {
+		id int64
+		cr time.Time
+		ud time.Time
+	}{
+		id: id,
+		cr: cr,
+		ud: ud,
+	}
+	return &s, nil
+}
+
+func ifErr() (_ bool, err error) {
+	defer try.Handle(&err, nil)
+	{
+		err := errors.New("test if")
+		try.Check(err)
+	}
+	return true, nil
+}
